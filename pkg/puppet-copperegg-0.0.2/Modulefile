d4184849fab8e7cf979340fe322200bfe8b66046
scott johnson <sjohnson@copperegg.com>
2012-07-11 12:53:05 -0500

    Commiting the built 0.0.2 release package

---
+++ pkg/puppet-copperegg-0.0.2/Modulefile
@@ -0,0 +1,7 @@
+name    'puppet-copperegg'
+version '0.0.2'
+author 'Scott C. Johnson'
+license 'BSD'
+summary 'Install and configure CopperEgg collector.'
+project_page 'https://github.com/'
+
---
+++ pkg/puppet-copperegg-0.0.2/manifests/params.pp
@@ -0,0 +1,20 @@
+# Class: copperegg::params
+#
+# This module manages the CopperEgg parameters
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class copperegg::params {
+    $tags = ""
+    $label = ""
+    $proxy = ""
+    $oom_protect = "false"
+    $use_fqdn = "false"
+}

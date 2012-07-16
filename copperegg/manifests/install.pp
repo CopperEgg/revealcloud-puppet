be6e2f7292e1ea8acd6f4af0386553362c4a8b04
Brett Maton <matonb@ltresources.co.uk>
2012-07-16 17:05:25 +0100

    Added RC_TAGS

--- copperegg/manifests/install.pp
+++ copperegg/manifests/install.pp
@@ -1,24 +1,19 @@
 class copperegg::install() {
   include copperegg::params
-
+  
   file { '/tmp/revealcloud' :
     ensure  => directory,
     mode    => "0755",
   }
 
   exec { 'Install RevealCloud Collector':
-    unless  => "/usr/bin/test -f /usr/local/revealcloud/revealcloud",
-    require => File [ '/tmp/revealcloud' ],
-    cwd     => "/tmp/revealcloud",
-    command => "/usr/bin/curl -s http://${copperegg::params::apikey}@api.copperegg.com/rc.sh | sh",
-    creates => "/usr/local/revealcloud/revealcloud",
+    unless      => "/usr/bin/test -f /usr/local/revealcloud/revealcloud",
+    require     => File [ '/tmp/revealcloud' ],
+    cwd         => "/tmp/revealcloud",
+    environment => "RC_TAG='${copperegg::params::revealCloudTags}'",
+    command     => "/usr/bin/curl -s http://${copperegg::params::revealCloudAPIKey}@api.copperegg.com/rc.sh | sh",
+    creates     => "/usr/local/revealcloud/revealcloud",
 
   }
 
-#  service { "revealcloud":
-#    name   => $service_name,    
-#    ensure => running,
-#    enable => true,
-#  }   
 }
-

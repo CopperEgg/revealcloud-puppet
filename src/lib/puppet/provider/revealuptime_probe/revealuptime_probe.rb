b3c0abfae84039674bef4f0a899583d2e9038a3c
Gagandeep Singh <gagandeep.singh@joshtechnologygroup.com>
2016-10-06 15:46:00 +0530

    CE-2326 Added relative_require in revealuptime_probe.rb

--- src/lib/puppet/provider/revealuptime_probe/revealuptime_probe.rb
+++ src/lib/puppet/provider/revealuptime_probe/revealuptime_probe.rb
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+require_relative 'api'
 
 Puppet::Type.type(:revealuptime_probe).provide(:revealuptime_probe) do
 
